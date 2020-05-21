a4acd01de64a6a48fd263d10d123a305b601547b
xufengFlynn <xzhang14@scu.edu>
2020-05-21 10:02:19 -0700

    Initial Project Add

---
+++ src/test/java/flynn/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package flynn.springframework.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
