019817d2625310833835dfd4a7c2394652f52476
xufengFlynn <xzhang14@scu.edu>
2020-05-21 11:28:59 -0700

    revise pom in root, add developerconnection

--- pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,9 +1,13 @@
 package sfpetclinic;
 
+import flynn.springframework.sfpetclinic.SfPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SfPetClinicApplication.class)
 class SfPetClinicApplicationTests {
 
     @Test
