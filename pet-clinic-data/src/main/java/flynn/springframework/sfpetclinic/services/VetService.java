8bfc2049a5820df6df61be9662e6187d7540914e
xufengFlynn <zhangxufeng26@gmail.com>
2020-05-26 01:27:46 -0700

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/VetService.java
@@ -3,13 +3,6 @@ package flynn.springframework.sfpetclinic.services;
 
 import flynn.springframework.sfpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
