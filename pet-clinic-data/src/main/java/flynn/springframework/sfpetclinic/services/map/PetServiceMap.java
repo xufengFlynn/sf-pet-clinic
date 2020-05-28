6404f8f6c1d8d8a5d3278430211ffb7e79d6ae66
xufengFlynn <zhangxufeng26@gmail.com>
2020-05-28 13:24:26 -0700

    Impl Map Based Services. closes #6

---
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package flynn.springframework.sfpetclinic.services.map;
+
+import flynn.springframework.sfpetclinic.model.Pet;
+import flynn.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstactMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
