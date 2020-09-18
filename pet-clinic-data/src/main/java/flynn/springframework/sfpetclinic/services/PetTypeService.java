5270fec3d91194e4f083fe938578af31cee8c66f
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-17 21:24:24 -0700

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package flynn.springframework.sfpetclinic.services;
+
+import flynn.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
