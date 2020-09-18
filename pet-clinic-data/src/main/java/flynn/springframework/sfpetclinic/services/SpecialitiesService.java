783d39d69b41f043278a596372a52f1ecdfba9e9
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-17 22:40:45 -0700

    Added Specialities Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package flynn.springframework.sfpetclinic.services;
+
+import flynn.springframework.sfpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package flynn.springframework.sfpetclinic.services.map;
+
+import flynn.springframework.sfpetclinic.model.Speciality;
+import flynn.springframework.sfpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialityServiceMap extends AbstactMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
