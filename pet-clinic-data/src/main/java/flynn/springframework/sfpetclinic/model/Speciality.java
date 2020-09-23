cd5e2e930153ee327156e33b4951161be8f87dad
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-23 16:44:31 -0700

    Added Project Lombok. closes #51

--- pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/flynn/springframework/sfpetclinic/model/Speciality.java
@@ -1,21 +1,20 @@
 package flynn.springframework.sfpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
